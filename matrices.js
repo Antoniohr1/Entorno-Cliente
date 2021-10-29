5d32c3691371f638d2b0dae1d89b16f9d0f49f4d
antoniohernandezruz2002@gmail.com <antoniohernandezruz2002@gmail.com>
2021-10-29 15:59:23 +0200

    bf

--- matrices.js
+++ matrices.js
@@ -43,10 +43,12 @@ if (selecion == 'M' || selecion == 'm') {
     let vmax = 10;
     //Variable que genera un numero aleatorio entre los valores de vmin y vmax
     let numRand = Math.floor(Math.random() * (vmax - (vmin) - 1) + vmin);
+    //Preguntamos los valores de las columnas y las filas de ambas matríces
     let filaMatriz1 = prompt("Introduce el número de filas de la matriz 1");
     let columnaMatriz1 = prompt("Introduce el número de columnas de la matriz 1");
     let filaMatriz2 = prompt("Introduce el número de filas de la matriz 2");
     let columnaMatriz2 = prompt("Introduce el número de columnas de la matriz 2");
+    //Si el valor de la columna de la matriz 1 es distinta al valor de la fila de la matriz 2 recarga la página
     if (columnaMatriz1 != filaMatriz2) {
         location.reload();
     } else {
@@ -63,7 +65,7 @@ if (selecion == 'M' || selecion == 'm') {
         }
         document.write("<br><br>");
         document.write("<h2>Segunda Matriz</h2>")
-            //Recorremos la primera matriz y  multiplicamos cada valor por la variable "real" y almacenamos el resultado en una posición de una nueva matriz que hemos creado
+            //Creamos e introducimos valores a la segunda matriz
         let matriz2 = new Array(filaMatriz2);
         for (let i = 0; i < filaMatriz2; i++) {
             matriz2[i] = new Array(columnaMatriz2);
@@ -74,18 +76,29 @@ if (selecion == 'M' || selecion == 'm') {
             document.write("<br>");
         }
         document.write("<br><br>");
+        //Incializamos la variable donde vamos a almacenar el resultado de la multiplicación
+        let resultado = 0;
         document.write("<h2>Matriz Resultado</h2>")
+            //Creamos e introducimos valores a la multiplicación y el ultimo for es para realizar la operación.
         let multiplicar = new Array(filaMatriz1);
-        for (n = 0; n < multiplicar.length; n++) {
+        for (n = 0; n < filaMatriz1; n++) {
             multiplicar[n] = new Array(columnaMatriz2);
-            for (y = 0; y < multiplicar[n].length; y++) {
+            for (y = 0; y < columnaMatriz2; y++) {
                 for (z = 0; z < columnaMatriz1; z++) {
-                    multiplicar[n][y] = multiplicar[n][y] + matriz[n][z] * matriz2[z][y];
-                    console.log(multiplicar[n][y]);
+                    resultado += matriz[n][z] * matriz2[z][y];
                 }
-
+                multiplicar[n][y] = resultado;
+                //Ponemos la variable a 0 para que no se sume con la siguiente operacion de la matriz.
+                resultado = 0;
             }
 
+        }
+        //Rescorremos la matriz multiplicación y mostramos el resultado
+        for (let i = 0; i < filaMatriz1; i++) {
+            for (let j = 0; j < columnaMatriz2; j++) {
+                document.write(multiplicar[i][j] + " ");
+            }
+            document.write("<br>")
         }
     }
 }
