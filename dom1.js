55dedb99911198d704a73eee87f83070da1bfd33
antoniohernandezruz2002@gmail.com <antoniohernandezruz2002@gmail.com>
2021-11-11 19:14:32 +0100

    clase jueves

---
+++ dom1.js
@@ -0,0 +1,94 @@
+'use strict'
+console.log(window.outerWidth + " - " + window.outerHeight);
+//window.open("ji ji ja ja");
+console.log(window.screen.width + " - " + window.screen.height); // Ancho de pantalla y alto (Resolución)
+console.log(window.screen.availWidth + " - " + window.screen.availHeight); // Excluyendo la barra del S.O
+console.log(window.navigator.userAgent); // Imprime la información del navegador
+/*window.navigator.geolocation.getCurrentPosition(function(position) { 
+    console.log("Latitude: " + position.coords.latitude + ", Longitude: " + position.coords.longitude);
+});*/
+
+
+/*setTimeout(() =>{
+    document.write("Se ha ejecutado el timer 1");
+},3000);
+
+let temporizador = setTimeout(() =>{
+    document.write("Se ha ejecutado el timer2");
+},5000);
+
+clearTimeout(temporizador);
+
+
+
+document.write("Hola");*/
+
+/*let tiempo=0;
+
+function f(){
+    tiempo++;
+    document.writeln("Han pasado " + tiempo + " segundos");
+    if(tiempo<10){
+        setTimeout(f,1000);
+    }
+}
+
+setTimeout(f,1000); 
+
+let a = 0;
+let intervalo = setInterval(() => {
+    a++;
+    console.log("Soy el intervalo " + a);
+    if(a===20)
+    clearInterval(intervalo);
+}, 1000);
+
+function multiplicar(a,b){
+    console.log(a*b);
+}
+
+setTimeout(multiplicar,3000,5,9);
+
+let variable = confirm("A ver que botón aprietas");
+console.log(variable);*/
+
+/*let elemento = document.getElementById("1");
+
+console.log(elemento);
+console.log(elemento.innerText);
+elemento.innerText = "Lo he cambiado";*/
+
+/*let parrafos = document.getElementsByTagName("p");
+for (let i = 0; i < parrafos.length; i++) {
+    console.log(parrafos[i]);
+}
+let elemento = document.getElementsByTagName("p");
+for (let i = 0; i <= elemento.length; i++) {
+    console.log(elemento[i].innerText);
+}
+elemento = [...elemento];
+elemento.forEach(item => { console.log(item.innerText) });
+//[...document.getElementsByTagName("p")].forEach(element=>{console.log(element.innerText)});
+let div = document.querySelector("div:nth-of-type(3n");
+[...div.children].forEach(item => { console.log(item.innerText) });*/
+
+let nuevoElemento = document.createElement("li");
+nuevoElemento.innerText = "Nuevo li";
+//FORMA 1
+let padre = document.getElementsByTagName("ul");
+padre[0].appendChild(nuevoElemento);
+
+//FORMA 2 
+let padre = document.querySelector("ul");
+padre.appendChild(nuevoElemento);
+
+//FORMA 3
+let padre = document.querySelectorAll("ul");
+padre[0].appendChild(nuevoElemento);
+
+//FORMA 4
+padre = document.body.children[0];
+padre.appendChild(nuevoElemento);
+
+let hermano = document.querySelector("li:nth-of-type(3n");
+padre.insertBefore(nuevoElemento, hermano);
--- ejemplo1.js
+++ ejemplo1.js
@@ -1,168 +1,312 @@
-/*'use strict'
-//Creo un elemento
-let a= 5;
+'use strict'
+
+//El código JavaScript vendrá aquí.
+//Creo un elemento  
+
+/*let v=document.createElement("p");
+v.textContent="Añadido dinámicamente"
+document.body.appendChild(v);*/
+
+/*let a=5;
 a=5;
 console.log(a);
-const b =6;
+const b=6;
 console.log(b);
+//b=7;
 
-//Declaración de función
+
+//Declaración de funciones
 function miFuncion(nombre) {
     console.log("Te llamas "+ nombre);
-
 }
-    miFuncion("Antonio")
-    console.log(miFuncion("Antonio"));
-//Declaración función anónima
+
+miFuncion("Antonio David");
+
+console.log(miFuncion("Antonio","David"));
+
+console.log(miFuncion);
+
+let g = miFuncion;
+g("pepito");
+
 let f = function (a,b) {
     return a*b;
 }
+
+console.log(f(2,5));
+
+f=(a)=>a*a;
+f=(a,b=1)=>a*b;
+
+f = (a,b)=>{
+    console.log("dentro de la funicón llamada");
+    a+b;
+}
 console.log(f(2,3));
 
-let t = (a,b)=> a+b;
-console.log(t(5,4));
 
-//Un pequeño ejemplo 
+function valorConIva(base, iva) {
+    return base+iva;
+}
 
-function tuEdad(edad) {
-    if(edad > 30 ){
-       return console.log("Eres mayor");
+console.log(valorConIva(100,21));
+
+
+let price = 65;
+
+function edad(a) {
+    if(a > 30){
+        console.log("estás viejo");
+    }else if(a > 50){
+        console.log("retirate")
     }else{
-     return console.log("Eres joven");
+        console.log("eres un chaval");
     }
 }
-console.log(tuEdad(40));*/
-/*
-let compMayor =(a,b) =>{
-    
-    if (a>b){
-   return a;
-}else {
-    return b;
+
+
+
+
+let compara = (a,b) => {
+    if (a>b) {
+       return a;
+    }
+    return b
 }
+
+let f = (a,b) => a>b ? a : b;
+console.log(f(f(9,4),1));
+
+if(temperatura){
+    console.log("Hace " + temperatura + " º");
+}else{
+    console.log("El agua se congela");
 }
-console.log(compMayor(compMayor(25,16),1));
-let dia =5;
+*/
+
+/*let dia=44;
+
 switch(dia){
     case 1:
         console.log("Lunes");
         break;
-        case 2:
+    case 2:
         console.log("Martes");
         break;
-        case 3:
-        console.log("Miércoles");
-        break;
-        case 4:
+    case 3:
+       console.log("Miercoles");
+       break;
+    case 4:
         console.log("Jueves");
         break;
-        case 5:
+    case 5:
         console.log("Viernes");
         break;
-        case 6:
+    case 6:
         console.log("Sabado");
         break;
-        case 7:
+    case 7:
         console.log("Domingo");
-        break;
-        default:
-            console.log("No es un día válido");
-            break;
-}
-*/
-/*let inicio =10;
-
-while (inicio>=0) {
-    console.log(inicio--);
-    
-}
-inicio=10;
- do{
-    console.log(inicio--);
-    
-}while (inicio>=0)*/
-
-/*for (let i=0 , j=10; i!=j; i++,j++){
-
+        break;  
+    default:
+        console.log("No es const element = array[index]un dia válido");
+        break;    
 }*/
 
-/*for(let i=1 ;i<=10;i++){
-    for(let j=1;j<=10; j++){
+
+/*let value=10;
+
+while (value >= 0) {
+    console.log(value--);
+}
+
+value=10;
+do{
+    console.log(value--);
+}while(value >=0)
+
+//while(a) console.log(--a);*/
+
+/*let z=0;
+for (let i = 1 ; i <= 10 ; i++) {
+    for (let j = 1; j <= 10 ; j++) {
         z++;
+        console.log(z);
     }
-}*/
+}
 
-/*let fila =10;
-let columna=10;
-document.write("<pre>")
-for(let i=1;i<=fila;i++){
-    for(let j=1;j<=columna;j++){
-           
-       if(i==0 || j== 0 || i==fila || j==fila){
-           document.write("#");
-       } else{
-           document.write(" ")
-       }   
-    }
-    document.write("<br>")
-}*/
+document.write("<pre>HOLA</pre>");*/
 
-/*let fila =10;
-let col = 10;
-document.write("<pre");
-for(let i = 0 ; i<=fila; i++){
-    for(let j = 0;j< 20 ; j++){
-        if(j>=(10 -i)&&j<=(10+i)){
+/*let fila=10;
+let col=10;
+
+document.write("<pre>");
+for (let i = 0; i <= fila; i++) {
+    for(let j = 0; j <=col; j++){
+        if (j==0|| i==0 || i== fila|| j==fila ) {
             document.write("#");
         }else{
             document.write(" ");
         }
-
     }
     document.write("<br>");
 }
-for(let i = 9; i>=0; i--){
+document.write("</pre>");*/
+
+/*let fila=10;
+let col=10;
+
+document.write("<pre>");
+for (let i = 0; i <= fila; i++) {
+    for(let j = 0; j < 20; j++){
+        if(j>=(10-i)&&j<=(10+i)){
+        document.write("#");
+        }else{
+            document.write(" ");
+        }
+    }
+    document.write("<br>");
+}
+
+for (let i = 9; i >= 0 ; i--){
     document.write(" ");
-    for(let j =19; j>=0;j--){
-        if(j>=(10 - i)&& j<=(10+i)){
+    for (let j = 19; j >=0; j--) {
+        if(j>=(10-i)&&j<=(10+i)){
             document.write("#");
-
-        }else{
-            document.write(" ");
-        }
+            }else{
+                document.write(" ");
+            }
     }
     document.write("<br>");
 }
-document.write("</pre>")
-
-console.log(Number.MIN_VALUE); // Imprime 5e-324 (El número más pequeño)
-console.log(Number.MAX_VALUE);  ; // Imprime 1.7976931348623157e+308 (El número más grande)
-console.log
-console.log(Number.MAX_VALUE * 2); // Imprime Infnity
-console.log(Number.POSITIVE_INFINITY); // Imprime Infnity
-console.log(Number.NEGATIVE_INFINITY); // Imprime -Infnity
-console.log(typeof Number.POSITIVE_INFINITY); // Imprime number*/
-/*let a=5;
-let b=-1;
-console.log(a &b);*/
-
-//Math.floor(Math.random()*(vmasc-vmin +1)+ vmin)
 
 
-/*let array = new Array(20);
-let vmin = 10;
-let vmax = 50;
-for (let i = 1; i < array.length; i++) {
-    array[i] = Math.floor(Math.random() * (vmax - vmin - 1) + vmin);
-    console.log(array[i]);
-}
-console.log("separacion")
-let mayor = 0;
 
-for (let i of array) {
-    if (i > mayor) {
-        mayor = i;
+document.write("</pre>");*/
+
+
+
+/*fila=10;
+col=10;
+
+document.write("<pre>");
+for (let i = 0; i <= fila; i++) {
+    for(let j = 0; j <=i; j++){
+        document.write("#");
     }
-
+    document.write("<br>");
 }
-console.log(mayor);*/
+document.write("</pre>");*/
+
+
+
+
+
+
+/*console.log(Number.MIN_VALUE); // Imprime 5e-324 (El número más pequeño)
+console.log(Number.MIN_SAFE_INTEGER);
+console.log(Number.MAX_VALUE); // Imprime 1.7976931348623157e+308 (El número más grande)
+console.log(Number.MAX_SAFE_INTEGER); */
+
+
+/*let a =1;
+console.log(a++);
+console.log(a);
+console.log(++a);*/
+
+
+/*let a = 5;
+let b = 9;
+console.log("Operacion de bits: " + (a<<1));  
+
+a=-1759390230;
+
+for (let i = 0; i <= 32; i++) {
+    console.log(a>>>=1);
+}*/
+
+
+/*let cadena = "ahola caracola";
+console.log(cadena.indexOf("a"));
+console.log(cadena.lastIndexOf("z"));
+for (let i = 0; i < cadena.length; i++) {
+    console.log(cadena.charAt(i));
+}
+
+let expR = cadena.match(/a/g);
+
+for (let i = 0; i < expR.length; i++) {
+    console.log(expR[i]);    
+}
+
+console.log(cadena.replace(/a/g,"i"));
+console.log(cadena);
+console.log(cadena.slice(8,10));
+console.log(cadena);*/
+
+/*let a = new Array(10);
+let vmin=50;
+let vmax=70;
+
+for (let i = 0; i < a.length; i++) {
+    a[i]=Math.floor(Math.random()*(vmax-vmin-1)+vmin); //como sacar un numero aleatorio
+    console.log(a[i]);
+}
+
+console.log("ZZZZZ");
+
+for(let i in a){
+    console.log(i);//solo muestra la posicion. Para mostrar valor a[i]
+}
+
+
+console.log("ZZZZZ");
+
+for(let i of a ){
+    console.log(i);
+}*/
+
+let a = new Array();
+let vmin=50;
+let vmax=70;
+
+for (let i = 0; i < a.length; i++) {
+    a[i]=Math.floor(Math.random()*(vmax-vmin-1)+vmin); //como sacar un numero aleatorio
+    console.log(a[i]);
+}
+console.log("ZZZZZ");
+
+let mayor=0;
+for(let i of a ){
+    if (i>mayor) {
+        mayor=i
+    }
+}
+
+console.log(mayor);
+
+/*let salir=true;
+let array = [];
+let dup=0;
+do {
+    let numero = prompt("Introduce un numero: ");
+    
+    if (array.length==0) {
+        array=numero;
+    }else{
+        for (let i = 0; i <= array.length; i++) {
+            if (numero>array[i]) {
+                array[i+1]=numero;
+            }
+        }
+    }
+ 
+    let salida = prompt("¿Quieres salir?")
+    if (salida=="si") {
+        salir = false;
+    }
+} while (salir!=false);
+
+for(let i of array ){
+    console.log(i);
+}*/
